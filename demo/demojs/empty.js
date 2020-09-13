f39d30eccc0c1be0842237698cd30f613f3b39f4
Oliver Schwartz <ollieschwartz99@gmail.com>
2020-09-13 16:46:11 +1000

    main.html skeleton, empty graph and js config file for experimentation

---
+++ demo/demojs/empty.js
@@ -0,0 +1,50 @@
+// development testing script - not used for library
+
+require.config({
+    baseUrl: "../js"
+  });
+  
+  
+  /*global require, KMap, $*/
+  require(["main"], function(KMap){
+    // create the model/settings so we can pass it into the views
+    var graphModel = new KMap.GraphModel(),
+        settings = {model: graphModel, useWisps: false,  graphDirection: "TB", showTransEdgesWisps: true};
+  
+    var graphView = new KMap.GraphView(settings),
+        graphListView = new KMap.GraphListView({model: graphModel});
+  
+    var handleDataFun = function (data) {
+  
+      // add the data to the graph model
+      graphModel.addJsonNodesToGraph(data);
+  
+      // set the graph placement (don't use if "x" and "y" are specified in the data)
+      graphView.optimizeGraphPlacement(false, false);
+  
+      // render the views
+      graphView.render();
+      graphListView.render();
+  
+      // insert them into the html
+      $("body").prepend(graphListView.$el);
+      $("#graph-view-wrapper").append(graphView.$el);
+  
+      // TODO integrate this into the view
+      var $wrap = $(document.body);
+      $wrap.height($(window).height());
+      $(window).resize(function () {
+        $wrap.height($(window).height());
+      });
+  
+      // center on a leaf element
+      var topoSortList = graphModel.getTopoSort();
+      graphView.centerForNode(graphModel.getNode(topoSortList[topoSortList.length -1]));
+  
+    };
+  
+    // fetch some graph data
+    $.getJSON("../data/empty.json", handleDataFun);
+  
+  });
+  
