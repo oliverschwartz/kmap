c1cc4df7f225ee6615eb5875eb9dac5197c96b84
Oliver Schwartz <ollieschwartz99@gmail.com>
2020-10-31 13:00:35 +1100

    graph model in models.py

--- project/models.py
+++ project/models.py
@@ -2,7 +2,14 @@ from flask_login import UserMixin
 from . import db
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+class Graph(db.Model): 
+    __tablename__ = 'graph'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    graph_text = db.Column(db.String(100))
