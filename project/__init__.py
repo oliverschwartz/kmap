9b183e10a1a9549d495b2b6314c45ee4fd87523c
Oliver Schwartz <ollieschwartz99@gmail.com>
2020-11-02 14:29:07 +1100

    static url fix

--- project/__init__.py
+++ project/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path="")
 
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
