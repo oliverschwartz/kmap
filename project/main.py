86d3bc9ed4199bdd64d80120212cb927e88d1a59
Oliver Schwartz <ollieschwartz99@gmail.com>
2020-11-02 14:38:02 +1100

    rendering graphs in index page

--- project/main.py
+++ project/main.py
@@ -12,21 +12,26 @@ def index():
     graphs = Graph.query.filter_by(user_id=current_user.id)
     return render_template('index.html', name=current_user.name, graphs=graphs)
 
+
 @main.route('/graph')
 def graph():
     return render_template('graph.html')
 
+
+# 'add-graph': endpoint for creating new graph files & adding them to the DB. 
 @main.route('/add-graph', methods=['POST'])
 @login_required
 def add_graph(): 
     filename = request.form['filename']
+    
     print('filename: ', filename)
     # TODO: create a graph object and save it to db
     graph = Graph(filename="filname", user_id=current_user.id, graph_text="some text")
     db.session.add(graph)
     db.session.commit()
-    
+
     return redirect(url_for("main.index"))
+
 
 @main.route('/profile')
 def profile(): 
